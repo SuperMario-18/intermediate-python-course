627613dbf5df4f94785af6ebc3fa03472332c325
SuperMario-18 <manizaleno_18@hotmail.com>
2020-08-07 15:58:17 -0400

    moved the "roll = random..." up a line

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,9 @@
 import random
-
 def main():
   roll = 5
+  roll = random.randint(1,6)
   print(f'You rolled a {roll}')
+
 
 if __name__== "__main__":
   main()
-  
-  roll = random.randint(1,6)
-  
-  
